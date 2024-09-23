309cba4170044237c2bc42e13a48e31429256014
John <cebotari@riseup.net>
2024-09-23 14:11:44 +0300

    implementation of fibonacci algo(iterative, matrix)

--- lab1.py
+++ lab1.py
@@ -1,9 +1,33 @@
+#metoda recursiva
 def fib1(n):
     if n<2: 
         return n
     else:
         return fib1(n-1)+fib1(n-2)
 
+#metoda iterativa
+def fib2(n):
+    i,j=1,0
+    for k in range(n):
+        j=i+j
+        i=j-i
+    return j
+
+#metoda bazata pe multiplicare de matrici
+def fib3(n):
+    i,j,k,h=1,0,0,1
+    while n>0:
+        if n%2==1:
+            t=j*h
+            j=i*h+j*k+t
+            i=i*k+t
+        t=h*h
+        h=2*k*h+t
+        k=k*k+t
+        n //= 2
+    return j
+
 
 for n in range(2,11):
-    print(str(fib1(n))+" ")
+    print(str(fib3(n))+" ")
+
