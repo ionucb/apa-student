674a21f404e5d3a8c5d4ecc3c6a1946fa5081ca3
John <cebotari@riseup.net>
2024-09-24 11:58:53 +0300

    add counters and testing them

--- lab1.py
+++ lab1.py
@@ -1,22 +1,33 @@
+count_rec=0
+count_mat=0
+count_it=0
+
 #metoda recursiva
 def fib1(n):
+    global count_rec
     if n<2: 
+        count_rec+=1
         return n
     else:
+        count_rec+=1
         return fib1(n-1)+fib1(n-2)
 
 #metoda iterativa
 def fib2(n):
-    i,j=1,0
+    global count_it
+    count_it,i,j=0,1,0
     for k in range(n):
         j=i+j
         i=j-i
+        count_it+=1
     return j
 
 #metoda bazata pe multiplicare de matrici
 def fib3(n):
+    global count_mat
     i,j,k,h=1,0,0,1
     while n>0:
+        count_mat+=1
         if n%2==1:
             t=j*h
             j=i*h+j*k+t
@@ -27,7 +38,13 @@ def fib3(n):
         n //= 2
     return j
 
+def main():
+    fib1(1)
+    print("recursiva:",count_rec)
+    fib2(5)
+    print("iterativa:",count_it)
+    fib3(5)
+    print("multiplicarea de matrici:",count_mat)
 
-for n in range(2,11):
-    print(str(fib3(n))+" ")
-
+if __name__ == "__main__":
+    main()
