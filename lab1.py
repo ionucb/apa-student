71d1f64eff7653e4f0e22a9778d343618aad33c3
John <cebotari@riseup.net>
2024-09-28 11:11:09 +0300

    menu modification

--- lab1.py
+++ lab1.py
@@ -1,19 +1,21 @@
+import time
+
 count_rec=0
 count_mat=0
 count_it=0
 
 #metoda recursiva
-def fib1(n):
+def fib_rec(n):
     global count_rec
     if n<2: 
         count_rec+=1
         return n
     else:
         count_rec+=1
-        return fib1(n-1)+fib1(n-2)
+        return fib_rec(n-1)+fib_rec(n-2)
 
 #metoda iterativa
-def fib2(n):
+def fib_it(n):
     global count_it
     count_it,i,j=0,1,0
     for k in range(n):
@@ -23,7 +25,7 @@ def fib2(n):
     return j
 
 #metoda bazata pe multiplicare de matrici
-def fib3(n):
+def fib_mat(n):
     global count_mat
     i,j,k,h=1,0,0,1
     while n>0:
@@ -38,52 +40,53 @@ def fib3(n):
         n //= 2
     return j
 
+def menu(n):
+    print("\nSelect the algorithm.")
+    print("1. Recursive.")
+    print("2. Iterative.")
+    print("3. Matrix exponentiatio.")
+    print("0. Exit")
+
+    choice = input("Please select an option: ")
+
+    if choice == '1':
+        print("\n----------------- RECURSIVE -----------------")
+        start_time = time.time()
+        print("fibonacci number is:",fib_rec(n))
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f'{count_rec} schimbări au avut loc în {execution_time:.4f} secunde.')
+
+    elif choice == '2':
+        print("\n----------------- ITERATIVE -----------------")
+        start_time = time.time()
+        print("fibonacci number is:",fib_it(n))
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f'{count_it} schimbări au avut loc în {execution_time:.4f} secunde.')
+
+    elif choice == '3':
+        print("\n----------------- MATRIX -----------------")
+        start_time = time.time()
+        print("fibonacci number is:",fib_mat(n))
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f'{count_mat} schimbări au avut loc în {execution_time:.4f} secunde.')
+
+    elif choice == '0':
+        exit()
+
+    else:
+        print("Invalid choise. Please select another option...")
+        menu(n)
+        
+
 def main():
-    global count_rec
-    global count_it
-    global count_mat
-    ans=True
-    while ans:
-        print ("""
-        1. fibonacci 10
-        2. fibonacci 100
-        3. fibonacci 1000
-        4. fibonacci 10000
-        5.Exit
-        """)
-        ans=input("What would you like to do? ") 
-        if ans=="1": 
-            fib1(10)
-            fib2(10)
-            fib3(10)
-            print("recursiv:",count_rec)
-            print("iterativ:",count_it)
-            print("matrici:",count_mat)
-        elif ans=="2":
-            fib1(100)
-            fib2(100)
-            fib3(100)
-            print("recursiv:",count_rec)
-            print("iterativ:",count_it)
-            print("matrici:",count_mat)
-        elif ans=="3":
-            fib1(1000)
-            fib2(1000)
-            fib3(1000)
-            print("recursiv:",count_rec)
-            print("iterativ:",count_it)
-            print("matrici:",count_mat)
-        elif ans=="4":
-            fib1(10000)
-            fib2(10000)
-            fib3(10000)
-            print("recursiv:",count_rec)
-            print("iterativ:",count_it)
-            print("matrici:",count_mat)
-        elif ans=="5":
-            print("\n Goodbye") 
-        elif ans !="":
-            print("\n Not Valid Choice Try again") 
+    n=int(input("which fibonacci number do you want to know?\n> "))
+    while True:
+        menu(n)
+
+
 
 if __name__ == "__main__":
     main()
